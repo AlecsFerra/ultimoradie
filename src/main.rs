be5a803436342f444246f1519eb7ed045f07cb3b
Alessio <35380179+AlecsFerra@users.noreply.github.com>
2022-06-17 01:49:50 +0200

    Added trimming to ids

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<(), Error> {
                                             .lines()
                                             .filter(|x| !x.is_empty())
                                             .filter(|x| !x.starts_with("#"))
-                                            .map(|x| x.parse().unwrap())
+                                            .map(|x| x.trim().parse().unwrap())
                                             .collect();
 
     println!("Loaded {} ids:", ultimora_cringe.len());
