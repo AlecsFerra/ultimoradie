31572b800f3771a454b74decf31b02f4e9d174d9
alecsferra <alecsferra@protonmail.com>
2022-06-21 21:46:50 +0200

    Handle bots

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,17 @@ async fn main() -> Result<(), Error> {
     println!("Starting");
 
     let ultimora_cringe: HashSet<i64> = fs::read_to_string("ultimora_cringe.txt")
-                                            .expect("ultimora_cringe.txt not found")
-                                            .lines()
-                                            .filter(|x| !x.is_empty())
-                                            .filter(|x| !x.starts_with("#"))
-                                            .map(|x| x.trim().parse().expect("One of the ids is malformed"))
-                                            .collect();
+        .expect("ultimora_cringe.txt not found")
+        .lines()
+        .filter(|x| !x.is_empty())
+        .filter(|x| !x.starts_with("#"))
+        .map(|x| x.trim().parse().expect("One of the ids is malformed"))
+        .collect();
 
     println!("Loaded {} ids:", ultimora_cringe.len());
     for &id in &ultimora_cringe {
         println!("{}", id);
     }
-
 
     println!("Started polling");
     let mut stream = api.stream();
@@ -31,23 +30,28 @@ async fn main() -> Result<(), Error> {
             Ok(update) => {
                 if let UpdateKind::Message(message) = &update.kind {
                     if let Some(forward) = &message.forward {
-                        if let ForwardFrom::Channel {
-                            channel,
-                            message_id: _,
-                        } = &forward.from
-                        {
-                            if ultimora_cringe.contains(&(channel.id.into())) {
-                                api.send(message.text_reply(format!(
-                                    "Ultim'ora detectato cringe eliminato, non provarci mai più {}",
-                                    message.from.first_name
-                                )))
+                        let should_delete = match &forward.from {
+                            ForwardFrom::Channel { channel, .. } => {
+                                ultimora_cringe.contains(&channel.id.into())
+                            }
+                            ForwardFrom::User { user } => {
+                                ultimora_cringe.contains(&user.id.into())
+                            }
+                            _ => false,
+                        };
+
+                        if should_delete {
+                            api.send(message.text_reply(format!(
+                                "Ultim'ora detectato cringe eliminato, non provarci mai più {}",
+                                message.from.first_name
+                            )))
+                            .await?;
+                            let deleted = api.send(message.delete()).await;
+                            if deleted.is_err() {
+                                api.send(message.text_reply(
+                                    "Volevo blastare questo cringe ma non sono abbastanza potente",
+                                ))
                                 .await?;
-                                let deleted = api.send(message.delete()).await;
-                                if deleted.is_err() {
-                                    api.send(message.text_reply(
-                                        "Volevo blastare questo cringe ma non sono abbastanza potente",
-                                    )).await?;
-                                }
                             }
                         }
                     }
